13c9b068fadbd98e9073db1d37946a6ed89b17f5
sahadan <sahadan@gmail.com>
2020-02-25 15:09:52 +0530

    First Commit

---
+++ DemoGitFirst/src/com/faith/app/ClinicApp.java
@@ -0,0 +1,12 @@
+package com.faith.app;
+
+public class ClinicApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Clinic App");
+
+	}
+
+}
